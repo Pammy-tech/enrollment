5a44060c193f3122339312be98751c6324f949d5
Pammy-tech <pamay0509@gmail.com>
2022-10-12 04:09:00 +0800

    commit

---
+++ pages/index.vue
@@ -0,0 +1,54 @@
+<template>
+<body class="bg">
+    <div class="container">
+        <div class="card shadow-lg o-hidden border-0 my-5">
+            <div class="card-body p-0">
+                <div class="row">
+                    <div class="col-lg-5 d-none d-lg-flex">
+                      <div class="image">
+                        <div class="imageoverlay">
+                        </div>
+                      </div>       
+                    </div>
+                    <div class="col-lg-7">
+                        <div class="p-5">
+                          <div class="text-center">
+                            <img class="rounded-sm" src="ctulogo.png" width="100px" height="100px">        
+                          </div> 
+                                <h4 class="text-dark mb-4 mt">LOGIN</h4>
+                                
+                            <form class="user">
+                                <div class="mb-3"><input class="form-control form-control-user" type="email" placeholder="Email Address/Username" name="email" /></div>
+                                <div class="mb-1"><input class="form-control form-control-user" type="password" placeholder="Password" name="password" /></div> 
+                                <div class="mb-3">
+                                  <div class="text-right">
+                                    <a class="small">Forgot Password?</a>
+                                  </div>
+                                </div>
+                                <div class="d-grid gap-2 col-6 mx-auto">
+                                  <button class="btn btn-outline-danger d-block btn-user mb-3 w-100" type="submit">Register Account</button>
+                                </div>
+                                <hr>
+                                <div class="text-center"> 
+                                  <router-link to="/register">No Account? Register here</router-link>
+                                  <a class="small">
+                                  </a>
+                                  
+                                </div>
+                                
+
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div> 
+</body>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
---
+++ pages/register.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="text-center"> 
+       <a class="small"> <router-link to="/index">Registration Page here</router-link>
+    
+        </a>
+
+        </div>
+</template>
+
+
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
