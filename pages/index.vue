fdc69b09d9092c6b117a99159abda32d201c965e
Pammy-tech <pamay0509@gmail.com>
2022-10-12 20:25:19 +0800

    Edit

--- pages/index.vue
+++ pages/index.vue
@@ -11,28 +11,28 @@
                       </div>       
                     </div>
                     <div class="col-lg-7">
-                        <div class="p-5">
-                          <div class="text-center">
-                            <img class="rounded-sm" src="ctulogo.png" width="100px" height="100px">        
-                          </div> 
-                                <h4 class="text-dark mb-4 mt">LOGIN</h4>
-                            <form class="user">
-                                <div class="mb-3"><input class="form-control form-control-user" type="email" placeholder="Email Address/Username" name="email" /></div>
-                                <div class="mb-1"><input class="form-control form-control-user" type="password" placeholder="Password" name="password" /></div> 
-                                <div class="mb-3">
-                                  <div class="text-right">
-                                    <a class="small">Forgot Password?</a>
-                                  </div>
-                                </div>
-                                <div class="d-grid gap-2 col-6 mx-auto">
-                                  <button class="btn btn-outline-danger d-block btn-user mb-3 w-100" type="submit">Register Account</button>
-                                </div>
-                                <hr>
-                                <div class="text-center"> 
-                                  <router-link to="/register">No Account? Register here</router-link>                                  
-                                </div>
-                            </form>
-                        </div>
+                      <div class="p-5">
+                        <div class="text-center">
+                          <img class="rounded-sm" src="ctulogo.png" width="100px" height="100px">        
+                        </div> 
+                        <h4 class="text-dark mb-4 mt">LOGIN</h4>
+                        <form class="user">
+                          <div class="mb-3"><input class="form-control form-control-user" type="email" placeholder="Email Address/Username" name="email" /></div>
+                          <div class="mb-1"><input class="form-control form-control-user" type="password" placeholder="Password" name="password" /></div> 
+                          <div class="mb-3">
+                            <div class="text-right">
+                              <a class="small">Forgot Password?</a>
+                            </div>
+                          </div>
+                          <div class="d-grid gap-2 col-6 mx-auto">
+                            <button class="btn btn-outline-danger d-block btn-user mb-3 w-100" type="submit">Register Account</button>
+                          </div>
+                          <hr>
+                          <div class="text-center"> 
+                            <router-link to="/register">No Account? Register here</router-link>                                  
+                          </div>
+                        </form>
+                      </div>
                     </div>
                 </div>
             </div>
