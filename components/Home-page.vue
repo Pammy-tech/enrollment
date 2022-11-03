4484697788b2ddf0ea1ef09652d3945992a1400f
Pamela May Tañedo <85401189+Pammy-tech@users.noreply.github.com>
2022-11-04 00:21:38 +0800

    Add

---
+++ components/Home-page.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <sidebar-page></sidebar-page>
+       
+        <div class="container" style="padding: 4rem; margin-left:250px">
+            <div>
+                <h3>Welcome to CTU Home</h3>
+            </div>
+        </div>
+    </div>
+</template>
+  
