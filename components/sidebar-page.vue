861ee891a816c1c2add94820457a598b6a20caca
Pamela May Tañedo <85401189+Pammy-tech@users.noreply.github.com>
2022-11-04 00:21:07 +0800

    Update sidebar-page.vue

--- components/sidebar-page.vue
+++ components/sidebar-page.vue
@@ -1,7 +1,7 @@
 <template>
     <nav id="sidebar" class="p-3" style="background-color: orange">
             <div class="sidebar-header">
-                <h3>Sidebar</h3>
+                <h3>Dashboard</h3>
             </div>
 
     <ul id="accordionSidebar" class="navbar-nav text-light">
@@ -11,15 +11,6 @@
     </NuxtLink> 
     <NuxtLink to="/dashboard">
         <li class="nav-item"><a class="nav-link"><i class="fas fa-user"></i><span>Dashboard</span></a></li>
-    </NuxtLink>
-    <NuxtLink to="/orders">
-        <li class="nav-item"><a class="nav-link"><i class="fas fa-table"></i><span>Orders</span></a></li>
-    </NuxtLink>
-    <NuxtLink to="/products">
-        <li class="nav-item"><a class="nav-link"><i class="far fa-user-circle"></i><span>Products</span></a></li>
-    </NuxtLink>
-    <NuxtLink to="/customer">
-        <li class="nav-item"><a class="nav-link"><i class="fas fa-user-circle"></i><span>Customer</span></a></li>
     </NuxtLink>
 </ul>           
 </nav>
